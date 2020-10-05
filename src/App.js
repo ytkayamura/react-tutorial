67e6e1980589dea38f68d8fada880d4858540551
綾村豊 <yutaka.ayamura@beeskynet.com>
2020-10-05 12:01:45 +0900

    change.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: "Dee",
+        job: "Aspring actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      },
+    ],
+  };
+
+  componentDidMount() {
+    const url =
+      "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => {
+        console.log(result);
+        this.setState({
+          data: result,
+        });
+      });
+  }
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  /*
+  render() {
+    const { data } = this.state;
+
+    const result = data
+      ? data.map((entry, index) => {
+          return <li key={index}>{entry}</li>;
+        })
+      : null;
+
+    return <ul>{result}</ul>;
+  }
+  */
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
